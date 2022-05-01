c50afcc2db8d120bacc55e746f20d174670c4668
unknown <kinekarpallavi21@gmail.com>
2022-05-01 17:36:12 +0530

    [Pallavi]Add.Calculate Daily Employee Wage based on part time or full time work using switchcase and get employee working hrs

--- employeewage.js
+++ employeewage.js
@@ -1,22 +1,28 @@
-
-//UC2-Calculate Daily Wage
+//UC3- Ability to Calculate Daily Employee Wage based on part time or full time work using switchcase and get employee hrs. 
+const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empWage =0;
-switch(empCheck){
-    case 1:
-        empWage = PART_TIME_HOURS*WAGE_PER_HOUR;
+let empHrs = 0;
+//Math.random() provides a random number from 0,1 ) 
+let empCheck = Math.floor(Math.random() * 3) ;
+switch (empCheck) {
+    case IS_ABSENT:
+        
+        empHrs = 0;
         break;
-    case 2:
-        empWage = FULL_TIME_HOURS*WAGE_PER_HOUR;
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS; //const PART_TIME_HOURS = 4;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS; //const PART_TIME_HOURS = 8;
         break;
     default:
-        empWage = 0;
         break;
 }
-console.log('Employee Wage is : '+ empWage);
+
+let empWage = empHrs * WAGE_PER_HOUR; //const WAGE_PER_HOUR = 20;
+console.log("Emp Wage: " + empWage);
+console.log("Emp Hours: " +empHrs);
