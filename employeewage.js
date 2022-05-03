35ea767830974154cb6a427f4b9a89948dfc4421
unknown <kinekarpallavi21@gmail.com>
2022-05-03 11:18:48 +0530

    [Pallavi]Add.Condition of maximum working hours

--- employeewage.js
+++ employeewage.js
@@ -1,37 +1,37 @@
-//UC4 -  Calculating  Wages for a month assuming 20 working days in a month
+//UC5-Calculating  Wages till a condition of total workinghours of 160 or max days of 20 is reached for a month
 const IS_ABSENT = 0;
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
-
-// function for  getworkinghours( parameter)
-function GetWorkingHours(empCheck){
-switch (empCheck) {
-    case IS_ABSENT:   //0    
-       return 0; // 0
-        break;
-    case IS_PART_TIME: //1
-        return PART_TIME_HOURS; //const PART_TIME_HOURS = 4;
-        break;
-    case IS_FULL_TIME: //2
-        return FULL_TIME_HOURS; //const PART_TIME_HOURS = 8;
-        break;
-    default:
-        return  null; //null 'no value'
-        
-   }
-}
+const WORKING_DAYS_IN_A_MONTH = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 let empHrs = 0;
-const WORKING_DAYS_IN_A_MONTH = 20;// assuming 20 working days in a month
-for (let days = 1; days <= WORKING_DAYS_IN_A_MONTH; days++) {
-//Math.random() provides a random number from 0,1,2 ) 
-let empCheck = Math.floor(Math.random() * 3) ;
-    empHrs += GetWorkingHours(empCheck);  // empHrs=empHrs+ empCheck random value
-}
-//calculate empWage
-let empWage = empHrs * WAGE_PER_HOUR;//const WAGE_PER_HOUR = 20;
+let dailyEmpHrs = 0;
 
-console.log("Total Hours: " + empHrs + "\nTotal Wage: " + empWage);
+while (totalEmpHrs <= MAX_WORKING_HOURS && totalWorkingDays <= MAX_WORKING_DAYS) {
+    
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() *3);
+    switch (empCheck) {
+        case IS_ABSENT:   //0    
+         dailyEmpHrs= 0; // 0
+            break;
+        case IS_PART_TIME: //1
+         dailyEmpHrs= PART_TIME_HOURS; //const PART_TIME_HOURS = 4;
+            break;
+        case IS_FULL_TIME: //2
+         dailyEmpHrs= FULL_TIME_HOURS; //const PART_TIME_HOURS = 8;
+            break;
+        default:
+            break
+    }
+    totalEmpHrs += dailyEmpHrs;
+    
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Hours: " +totalEmpHrs+ "\nTotal Wage: " + empWage);
